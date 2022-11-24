7b7e05e2564d580531517340d41fc942861d6f5b
Ashish Ranjan <ashish_ranjan08@hotmail.com>
2022-11-24 11:44:19 +0530

    get cname from the form.

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ const app = express()
 app.get("/runform", (req, res) => {
     res.sendFile(__dirname + "/rundocker.html");
 })
+
+app.get("/run", (req,res) => {
+    const cname = req.query.cname;
+    const cimage = req.query.cimage;
+
+    res.send(cimage);
+})
 app.listen(3000, () => {console.log("container app tool started.")})
