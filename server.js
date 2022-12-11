e305ff97055e1b902f8d8f5abf299eb79d244f6e
Ashish Ranjan <akp9801@gmail.com>
2022-12-11 15:36:54 +0530

    accessing command name from the form

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { exec } = require("child_process")
+
 const express = require("express")
 const app = express()
 
@@ -7,8 +9,12 @@ app.get("/runform", (req, res) => {
 
 app.get("/run", (req,res) => {
     const cname = req.query.cname;
-    const cimage = req.query.cimage;
+    // const cimage = req.query.cimage;
 
-    res.send(cimage);
+    // res.send(cimage);
+    exec(cname, (err, stdout, stderr) => {
+        console.log(stdout);
+        res.send("<pre>" + stdout + "</pre>");
+    })
 })
 app.listen(3000, () => {console.log("container app tool started.")})
